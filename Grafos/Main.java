a0169a514ef7648d4749c59d1512a9dd6bef5bf0
Isac Kaik <isackaik1@gmail.com>
2023-03-23 16:53:46 -0300

    DFS em dígrafos

--- Grafos/Main.java
+++ Grafos/Main.java
@@ -1,24 +1,18 @@
-import java.util.*;
-
 public class Main {
 
     public static void main(String[] args) {
-        Scanner inputs = new Scanner(System.in);
-        Deque<Integer> deck = new ArrayDeque<>();
 
-        int n = inputs.nextInt();
-        int q = inputs.nextInt();
+        int[][] matriz = {
+                { 0, 1, 1, 0, 0, 1, 0 },
+                { 0, 0, 0, 0, 1, 0, 0 },
+                { 0, 0, 0, 1, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 1, 1 },
+                { 0, 0, 0, 0, 0, 1, 0 },
+                { 0, 0, 1, 0, 0, 0, 1 },
+                { 0, 0, 0, 0, 0, 0, 0 },
+        };
 
-        for (int i = 0; i < n; i++)
-            deck.add(inputs.nextInt());
-
-        for (int i = 0; i < q; i++){
-            int aux = inputs.nextInt();
-            ArrayList<Integer> deck2 = new ArrayList<>(deck);
-            System.out.println(deck2.indexOf(aux) + 1);
-            deck.remove(aux);
-            deck.addFirst(aux);
-        }
-        inputs.close();
+        GrafoDigrafo grafo = new GrafoDigrafo(7);
+        grafo.dfsDigrafo(matriz, 1);
     }
 }
